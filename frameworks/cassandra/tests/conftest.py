0dcd907a7353cd6764bc24d03acfe4b0eed7f7d8
Kevin Wood <kwood@mesosphere.com>
2017-06-29 09:47:56 -0500

    random package name; try/finally instead of addfinalizer

--- frameworks/cassandra/tests/conftest.py
+++ frameworks/cassandra/tests/conftest.py
@@ -1,5 +1,8 @@
-import os
 import json
+import os
+import random
+import string
+
 import pytest
 import shakedown
 
@@ -13,25 +16,27 @@ def configure_universe(request):
     if not stub_universe_urls:
         return
     for url in stub_universe_urls.split(' '):
-        package_name = url.split('/')[4] # TODO: does this need to be a random string?
+        package_name = 'testpkg-'
+        package_name += ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(8))
         stub_urls[package_name] = url
 
-    # clear out the added universe repositores at testing end
-    def unconfigure_universe():
+    try:
+        # clean up any duplicate repositories
+        current_universes = shakedown.run_dcos_command('dcos package repo list --json')
+        for repo in json.loads(current_universes)['repositories']:
+            if repo['uri'] in stub_urls.values():
+                remove_package_cmd = 'dcos package repo remove {}'.format(repo['name'])
+                shakedown.run_dcos_command(remove_package_cmd)
+
+        # add the needed universe repositories
+        for name, url in stub_urls.items():
+            add_package_cmd = 'dcos package repo add --index=0 {} {}'.format(name, url)
+            shakedown.run_dcos_command(add_package_cmd)
+
+        # let the test session execute
+        yield
+    finally:
+        # clear out the added universe repositores at testing end
         for name, url in stub_urls.items():
             remove_package_cmd = 'dcos package repo remove {}'.format(name)
             shakedown.run_dcos_command(remove_package_cmd)
-    request.addfinalizer(unconfigure_universe)
-
-    # clean up any duplicate repositories
-    # TODO: is this needed, or do duplicate repositories signify an actual error?
-    current_universes = shakedown.run_dcos_command('dcos package repo list --json')
-    for repo in json.loads(current_universes)['repositories']:
-        if repo['uri'] in stub_urls.values():
-            remove_package_cmd = 'dcos package repo remove {}'.format(repo['name'])
-            shakedown.run_dcos_command(remove_package_cmd)
-
-    # add the needed universe repositories
-    for name, url in stub_urls.items():
-        add_package_cmd = 'dcos package repo add --index=0 {} {}'.format(name, url)
-        shakedown.run_dcos_command(add_package_cmd)
